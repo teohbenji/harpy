0e78f963627eb65c896c06efffe1bb1dd6710627
teohbenji <teohbenjamin1@gmail.com>
2023-04-05 09:40:54 +0800

    tested 7 lever switches with notes

--- harpy/harpy.ino
+++ harpy/harpy.ino
@@ -4,27 +4,32 @@
 #include <FastLED.h>
 #include <ezButton.h>
 
-#define LED_PIN_1     4
-#define LED_PIN_2     52
-#define NUM_LEDS    1
+#define LED_PIN_7    2
+#define NUM_LEDS    26
 
-CRGB leds_1[NUM_LEDS];
-CRGB leds_2[NUM_LEDS];
+CRGB leds_7[NUM_LEDS];
+
 ezButton limitSwitch1(13);  
-ezButton limitSwitch2(12);  
-ezButton limitSwitch3(11);
-ezButton limitSwitch4(10);  
-ezButton limitSwitch5(9);  
-ezButton limitSwitch6(8);  
-ezButton limitSwitch7(7);  
-int switchesArr[] = {}; //List of switches
+ezButton limitSwitch2(11);  
+ezButton limitSwitch3(9);
+ezButton limitSwitch4(7);  
+ezButton limitSwitch5(5);  
+ezButton limitSwitch6(3);  
+ezButton limitSwitch7(4);  
 
 void setup() {
-  FastLED.addLeds<WS2812, LED_PIN_1, GRB>(leds_1, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_PIN_2, GRB>(leds_2, NUM_LEDS);
   Serial.begin(9600); //Baud rate has to match Python
+  
+  FastLED.addLeds<WS2812, LED_PIN_7, GRB>(leds_7, NUM_LEDS);
+  FastLED.setBrightness(122);
+  
   limitSwitch1.setDebounceTime(50); // set debounce time to 50 milliseconds
   limitSwitch2.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch3.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch4.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch5.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch6.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch7.setDebounceTime(50); // set debounce time to 50 milliseconds
 }
 
 void loop() {
@@ -40,162 +45,139 @@ void loop() {
 void detectString1Pull(){
   limitSwitch1.loop();
   int state = limitSwitch1.getState();
-  Serial.println(state);
+  int rgb[] = {0, 0, 0};
   
-  if (state == 0){
+  if(state == 0) {
         Serial.println("at rest");
-  }
-  else{
-    while (state == 1){
-        Serial.println("C");
-        limitSwitch1.loop();
-        state = limitSwitch1.getState();
+      }
+  else {
+    while(state == 1) {
+      Serial.println("C");
+      limitSwitch1.loop();
+      state = limitSwitch1.getState();
     }
-  //   delay(100);
-  //   Serial.println("released");
-  //   leds_1[0] = CRGB(255, 0, 0);
-  //   FastLED.show();
-  //   delay(250);
-  //   leds_1[0] = CRGB(0, 0, 0);
-  //   FastLED.show();
-  //   delay(250);
+    // blinkLedStrip(leds_1, rgb);
   }
 }
 
 void detectString2Pull(){
   limitSwitch2.loop();
   int state = limitSwitch2.getState();
+  int rgb[] = {0, 0, 0};
   
-  if (state == 0){
+  if(state == 0) {
         Serial.println("at rest");
       }
-  else{
-      while (state == 1){
-        Serial.println("D");
-        limitSwitch2.loop();
-        state = limitSwitch2.getState();
+  else {
+    while(state == 1) {
+      Serial.println("D");
+      limitSwitch2.loop();
+      state = limitSwitch2.getState();
     }
-    //   delay(100);
-    //   Serial.println("released");
-    //   leds_2[0] = CRGB(255, 0, 0);
-    //   FastLED.show();
-    //   delay(250);
-    //   leds_2[0] = CRGB(0, 0, 0);
-    //   FastLED.show();
-    }
+    // blinkLedStrip(leds_2, rgb);
+  }
 }
 
 void detectString3Pull()  {
   limitSwitch3.loop();
   int state = limitSwitch3.getState();
+  int rgb[] = {0, 0, 0};
   
-  if (state == 0){
+  if(state == 0) {
         Serial.println("at rest");
       }
-  else{
-      while (state == 1){
-        Serial.println("E");
-        limitSwitch3.loop();
-        state = limitSwitch3.getState();
+  else {
+    while(state == 1) {
+      Serial.println("E");
+      limitSwitch3.loop();
+      state = limitSwitch3.getState();
     }
-    //   delay(100);
-    //   Serial.println("released");
-    //   leds_2[0] = CRGB(255, 0, 0);
-    //   FastLED.show();
-    //   delay(250);
-    //   leds_2[0] = CRGB(0, 0, 0);
-    //   FastLED.show();
-    }
+    // blinkLedStrip(leds_3, rgb);
+  }
 }
 
 void detectString4Pull()  {
   limitSwitch4.loop();
   int state = limitSwitch4.getState();
+  int rgb[] = {0, 0, 0};
   
-  if (state == 0){
+  if(state == 0) {
         Serial.println("at rest");
       }
-  else{
-      while (state == 1){
-        Serial.println("F");
-        limitSwitch4.loop();
-        state = limitSwitch4.getState();
+  else {
+    while(state == 1) {
+      Serial.println("F");
+      limitSwitch4.loop();
+      state = limitSwitch4.getState();
     }
-    //   delay(100);
-    //   Serial.println("released");
-    //   leds_2[0] = CRGB(255, 0, 0);
-    //   FastLED.show();
-    //   delay(250);
-    //   leds_2[0] = CRGB(0, 0, 0);
-    //   FastLED.show();
-    }
+    // blinkLedStrip(leds_4, rgb);
+  }
 }
 
 void detectString5Pull()  {
   limitSwitch5.loop();
   int state = limitSwitch5.getState();
+  int rgb[] = {0, 0, 0};
   
-  if (state == 0){
+  if(state == 0) {
         Serial.println("at rest");
       }
-  else{
-      while (state == 1){
-        Serial.println("G");
-        limitSwitch5.loop();
-        state = limitSwitch5.getState();
+  else {
+    while(state == 1) {
+      Serial.println("G");
+      limitSwitch5.loop();
+      state = limitSwitch5.getState();
     }
-    //   delay(100);
-    //   Serial.println("released");
-    //   leds_2[0] = CRGB(255, 0, 0);
-    //   FastLED.show();
-    //   delay(250);
-    //   leds_2[0] = CRGB(0, 0, 0);
-    //   FastLED.show();
-    }
+    // blinkLedStrip(leds_5, rgb);
+  }
 }
 
-void detectString6Pull()  {
+void detectString6Pull() {
   limitSwitch6.loop();
   int state = limitSwitch6.getState();
+  int rgb[] = {246, 190, 0};
   
-  if (state == 0){
+  if(state == 0) {
         Serial.println("at rest");
       }
-  else{
-      while (state == 1){
-        Serial.println("A");
-        limitSwitch6.loop();
-        state = limitSwitch6.getState();
+  else {
+    while(state == 1) {
+      Serial.println("A");
+      limitSwitch6.loop();
+      state = limitSwitch6.getState();
     }
-    //   delay(100);
-    //   Serial.println("released");
-    //   leds_2[0] = CRGB(255, 0, 0);
-    //   FastLED.show();
-    //   delay(250);
-    //   leds_2[0] = CRGB(0, 0, 0);
-    //   FastLED.show();
-    }
+    // blinkLedStrip(leds_6, rgb);
+  }
 }
 
-void detectString7Pull()  {
+void detectString7Pull() {
   limitSwitch7.loop();
   int state = limitSwitch7.getState();
+  int rgb[] = {1, 50, 32};
   
-  if (state == 0){
+  if (state == 0) {
         Serial.println("at rest");
       }
-  else{
-      while (state == 1){
-        Serial.println("B");
-        limitSwitch7.loop();
-        state = limitSwitch7.getState();
+  else {
+    while(state == 1) {
+      Serial.println("B");
+      limitSwitch7.loop();
+      state = limitSwitch7.getState();
     }
-    //   delay(100);
-    //   Serial.println("released");
-    //   leds_2[0] = CRGB(255, 0, 0);
-    //   FastLED.show();
-    //   delay(250);
-    //   leds_2[0] = CRGB(0, 0, 0);
-    //   FastLED.show();
-    }
+    blinkLedStrip(leds_7, rgb);
+  }
+}
+
+void blinkLedStrip(CRGB leds[], int rgb[]) {
+  //turns on LED for 0.25s
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(rgb[0], rgb[1], rgb[2]);
+  }
+  FastLED.show();
+  delay(500);
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
 }
