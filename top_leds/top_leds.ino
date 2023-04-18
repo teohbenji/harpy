8449af1fe5d8d5309efca84b3a4f68dcfe198ec5
teohbenji <teohbenjamin1@gmail.com>
2023-04-18 15:27:33 +0800

    Finished top_leds code

--- top_leds/top_leds.ino
+++ top_leds/top_leds.ino
@@ -1,43 +1,28 @@
 #include <FastLED.h>
 
 //Display LEDs
-#define DISPLAY_LED_PIN_1    2
-#define DISPLAY_LED_PIN_2    3
-#define DISPLAY_LED_PIN_3    4
-#define DISPLAY_LED_PIN_4    5
+#define LED_PIN    2
+#define NUM_LEDS   26
 
-#define NUM_DISPLAY_LEDS      26
-
-int count = 0;
-
-//Display leds #REMEMBER TO CHANGE LED NUM
-CRGB displayLEDs1[NUM_DISPLAY_LEDS];
-CRGB displayLEDs2[NUM_DISPLAY_LEDS];
-CRGB displayLEDs3[NUM_DISPLAY_LEDS];
-CRGB displayLEDs4[NUM_DISPLAY_LEDS];
+CRGB leds[NUM_LEDS];
 
 void setup() {
   //Display LEDs
-  FastLED.addLeds<WS2812, DISPLAY_LED_PIN_1, GRB>(displayLEDs1, NUM_DISPLAY_LEDS);
-  FastLED.addLeds<WS2812, DISPLAY_LED_PIN_2, GRB>(displayLEDs2, NUM_DISPLAY_LEDS);
-  FastLED.addLeds<WS2812, DISPLAY_LED_PIN_3, GRB>(displayLEDs3, NUM_DISPLAY_LEDS);
-  FastLED.addLeds<WS2812, DISPLAY_LED_PIN_4, GRB>(displayLEDs4, NUM_DISPLAY_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  turnOnTopLEDs();
+}
 
+void turnOnSpecificLEDStrip(CRGB leds[], int rgb[]) {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(rgb[0], rgb[1], rgb[2]);
+  }
+  FastLED.show();
 }
 
 void turnOnTopLEDs(){
   int rgb[] = {255,127,0};
-  turnOnSpecificLEDStrip(displayLEDs1, rgb);
-  turnOnSpecificLEDStrip(displayLEDs2, rgb);
-  turnOnSpecificLEDStrip(displayLEDs3, rgb);
-  turnOnSpecificLEDStrip(displayLEDs4, rgb);  
+  turnOnSpecificLEDStrip(leds, rgb);
 }
 
 void loop() {
-  //Turn on display leds
-  if(count == 0){
-    turnOnTopLEDs();
-    count = 1;
-  }
-
 }
