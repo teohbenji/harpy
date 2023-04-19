bb808ba6e04e126ed3e4f78d3afc48d3d63723f5
teohbenji <teohbenjamin1@gmail.com>
2023-04-20 03:21:22 +0800

    updated top leds code

--- top_leds/top_leds.ino
+++ top_leds/top_leds.ino
@@ -2,27 +2,19 @@
 
 //Display LEDs
 #define LED_PIN    2
-#define NUM_LEDS   26
+#define NUM_LEDS   240
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   //Display LEDs
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  turnOnTopLEDs();
-}
-
-void turnOnSpecificLEDStrip(CRGB leds[], int rgb[]) {
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(rgb[0], rgb[1], rgb[2]);
-  }
-  FastLED.show();
-}
-
-void turnOnTopLEDs(){
-  int rgb[] = {255,127,0};
-  turnOnSpecificLEDStrip(leds, rgb);
+  FastLED.setBrightness(5);
 }
 
 void loop() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB(255, 255, 255);
+  }
+  FastLED.show();
 }
